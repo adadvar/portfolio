78f3b7ae80881e63a31e3bf27d18533b4b221d68
adadvar <alirezadadvar24@gmail.com>
2025-02-11 20:16:37 +0330

    change header with observer, and add custom hook for do it

--- context/active-section-context.tsx
+++ context/active-section-context.tsx
@@ -1,15 +1,16 @@
 "use client";
 
+import type { SectionName } from "@/lib/types";
 import React, { createContext, useContext, useState } from "react";
-import { links } from "@/lib/data";
 
-type SectionName = (typeof links)[number]["name"];
 type ActiveSectionContextProviderProps = {
   children: React.ReactNode;
 };
 type ActiveSectionContextType = {
   activeSection: SectionName;
   setActiveSection: React.Dispatch<React.SetStateAction<SectionName>>;
+  timeOfLastClick: number;
+  setTimeOfLastClick: React.Dispatch<React.SetStateAction<number>>;
 };
 
 export const ActiveSectionContext =
@@ -19,9 +20,17 @@ export default function ActiveSectionContextProvider({
   children,
 }: ActiveSectionContextProviderProps) {
   const [activeSection, setActiveSection] = useState<SectionName>("Home");
+  const [timeOfLastClick, setTimeOfLastClick] = useState(0); // we need to keep track of this to disable the observer temporarily when the user clicks on a link
 
   return (
-    <ActiveSectionContext.Provider value={{ activeSection, setActiveSection }}>
+    <ActiveSectionContext.Provider
+      value={{
+        activeSection,
+        setActiveSection,
+        timeOfLastClick,
+        setTimeOfLastClick,
+      }}
+    >
       {children}
     </ActiveSectionContext.Provider>
   );
