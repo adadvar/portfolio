e60beeb886ae3527028e63b8b7d689feccdad07b
adadvar <alirezadadvar24@gmail.com>
2025-10-20 11:00:27 +0330

    add phone to intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import photo from "@/public/myphoto.jpg";
 import { motion } from "framer-motion";
 import Link from "next/link";
-import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { BsArrowRight, BsLinkedin, BsTelephoneFill } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
@@ -107,6 +107,13 @@ export default function Intro() {
           target="_blank"
         >
           <FaGithubSquare />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="tel:+989305947825"
+        >
+          <BsTelephoneFill />
         </a>
       </motion.div>
     </section>
